8db5fc9390056ba575f46bd65017779212d2b719
Tomasz Proczkowski <proczek@gmail.com>
2020-01-14 22:31:28 +0100

    copy constructor

--- constructors.cpp
+++ constructors.cpp
@@ -1,9 +1,14 @@
 //clang constructors.cpp -Wall -Wdeprecated -lstdc++ --std=c++2a
+#include <iostream>
 
 class A
 {
 public:
     virtual ~A() = default;
+    A(const A&)
+    {
+        std::cout << "Copy constructor A" << std::endl;
+    }
 };
 
 int main()
